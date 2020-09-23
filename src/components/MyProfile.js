6ad477efce8c7332e7160fd4cb03e35e57e29090
wyao <289672494@qq.com>
2020-09-23 17:09:39 +0800

    [wang yao] finish 基本

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -2,9 +2,64 @@ import React, {Component} from 'react';
 import './myProfile.less';
 
 class MyProfile extends Component {
+  state = {
+    name: '',
+    gender: 'Male',
+    description: '',
+    haveRead: false
+  }
+
+  onChange = (field, event, checked=false) => {
+    const value = checked === false ? event.target.value : event.target.checked;
+    this.setState({
+      [field]: value
+    })
+  }
+
+  onSubmit = (e) => {
+    console.log(this.state);
+    
+  }
+
   render() {
     return (
-      <form></form>
+      <form target="test">
+        <div className="content">
+          <h2>My Profile</h2>
+          
+          <div className='form-item'>
+            <span>Name</span>
+            <input type="text" placeholder='Your name' onChange={(e) => this.onChange('name', e)} />
+          </div>
+
+          <div className='form-item'>
+            <span>Gender</span>
+            <select onChange={(e) => this.onChange('gender', e)}>
+              <option value="Male" defaultValue>Male</option>
+              <option value="Male">Female</option>
+            </select>
+          </div>
+
+          <div className='form-item'>
+            <span>Description</span>
+            <textarea placeholder='Description about your self' rows="10" onChange={(e) => this.onChange('description', e)}></textarea>
+          </div>
+
+          <div className='checked-item'>
+            <input type="checkbox" name="" id="" onChange={(e) => this.onChange('haveRead', e, true)} />
+            I have read the terms of conduct
+          </div>
+
+          <div align='center'>
+            <input onClick={this.onSubmit} type="submit" value="Submit" disabled={
+              this.state.name === '' ||
+              this.state.gender === '' || 
+              this.state.description === '' || 
+              this.state.haveRead === false
+            } />
+          </div>
+        </div>
+      </form>
     );
   }
 }
